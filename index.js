35329ab60bbb1e277d045bef762ba33454085b17
Egor Bokov <s1lice.egor@gmail.com>
2021-09-14 14:44:40 +0300

    Changed Functions

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 "use strict";
 let isResult;
-let text = document.getElementById('outtext');
+const text = document.getElementById('outtext');
+const operators = [
+    '*',
+    '/',
+    '+',
+    '-',
+    '.',
+    '%',
+];
 
-// Назначение функции очистки поля ввода 
-let ac = document.getElementById('ac');
-ac.addEventListener('click', clearInput);
+let clearAllButton = document.getElementById('ac');
+clearAllButton.addEventListener('click', clearInput);
 
-
-// Очистка поля ввода
 function clearInput() {
     text.innerHTML = '';
 }
 
-// Каждому элементу добваить onclick функцию
 
 let numbuttList = document.querySelectorAll('.numbutt');
 let arrList = Array.from(numbuttList);
-for ( let every of arrList ) {
-    every.addEventListener('click',adder);
-} 
-
-// Функция добавления в поле ввода asdsad
-function adder() {
-text.innerHTML += event.target.innerHTML;
+for (let every of arrList) {
+    every.addEventListener('click', adder);
 }
 
+function adder(event) {
+    const value = event.target.innerHTML;
+    let lastChar = text.innerHTML[text.innerHTML.length - 1];
+    let lastCharResult, valueResult;
 
-// Тест Юриного метода 
-let final = eval('10*2');
-console.log(final);
+    operators.map((op) => {
+        if (op === value) valueResult = true
+    })
 
+    operators.map((op) => {
+        if (op === lastChar) lastCharResult = true
+    })
 
-// Равно
+    if (!(valueResult && (lastCharResult || lastChar == ''))) {
+        text.innerHTML += event.target.innerHTML;
+    }
+}
+
 let resButt = document.querySelector('.resButt');
 resButt.addEventListener('click', result);
 
 function result() {
-    text.innerHTML = eval(text.innerHTML); 
+    text.innerHTML = eval(text.innerHTML);
 }
 
-// Корректор
+
 let correctButt = document.querySelector('.correctButt');
 correctButt.addEventListener('click', correcter);
 
 function correcter() {
-text.innerHTML = text.innerHTML.slice(0, text.innerHTML.length -1 );
+    text.innerHTML = text.innerHTML.slice(0, text.innerHTML.length - 1);
 }
